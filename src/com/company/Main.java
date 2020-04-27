6da93e6d6b08e92b2732884958a861122e99607e
JakubWrzesniak <jakub.wrzesniak@outlook.com>
2020-04-27 16:00:52 +0200

    Create Simulation object

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
 
+import ReplacementAlgorithm.Simulation;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Simulation simulation = new Simulation(4,10);
+        simulation.RunAlgorithms();
     }
 }
