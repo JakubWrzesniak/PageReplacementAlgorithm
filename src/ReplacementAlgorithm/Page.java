bdbafa6a471665772e4f33ef308c4d62c444bad0
JakubWrzesniak <jakub.wrzesniak@outlook.com>
2020-04-27 14:55:42 +0200

    Edit Page Class

--- src/ReplacementAlgorithm/Page.java
+++ src/ReplacementAlgorithm/Page.java
@@ -1,4 +1,47 @@
 package ReplacementAlgorithm;
 
-public class Page {
+public class Page implements Comparable<Page> {
+    private int number;
+    private boolean mifBit; // Bit modyfikacji
+    private Integer reference;
+
+    public Page(int number, boolean mifBit, int reference) {
+        this.number = number;
+        this.mifBit = mifBit;
+        this.reference = reference;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public boolean isMifBit() {
+        return mifBit;
+    }
+
+    public void setMifBit(boolean mifBit) {
+        this.mifBit = mifBit;
+    }
+
+    public int getReference() {
+        return reference;
+    }
+
+    public void setReference(int reference) {
+        this.reference = reference;
+    }
+
+    @Override
+    public int compareTo(Page o) {
+        return reference.compareTo(o.getReference());
+    }
+
+    @Override
+    public String toString() {
+        return number + " ";
+    }
 }
